1e40da1f0f7328b99bd52c9a7efc1dc7c96e97e1
Laura <lauraporto596@gmail.com>
2021-02-21 19:46:11 +0100

    setting up DB

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const filmDb = require("./storage/filmDb");
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
 
 //RUTAS
 //Importación de las rutas
@@ -11,7 +13,6 @@ let filmRouter = require("./routers/filmRouter");
 let userRouter = require("./routers/userRouter");
 let orderRouter = require("./routers/orderRouter");
 
-//Endpoints
 app.use("/film", filmRouter);
 app.use("/user", userRouter);
 app.use("/order", orderRouter);
@@ -19,6 +20,9 @@ app.use("/order", orderRouter);
 
 //Levantamos servidor
 // npm start , npm run dev
-app.listen(port, () => {
-    console.log(`Server listening at https://localhost:${port}`);
-});
+filmDb
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server listening at https://localhost:${port}`);
+        })
+    }).catch((error) => console.log(error.message));
--- storage/filmDb.js
+++ storage/filmDb.js
@@ -1,1 +1,24 @@
-module.exports = filmDb.js;
+const logger = (log) => { console.log(new Date, log) };
+
+const mongoose = require("mongoose");
+
+const MONGO_HOST = process.env.MONGO_HOST || "localhost";
+const MONGO_PORT = process.env.MONGO_PORT || "27017";
+const MONGO_DBNAME = process.env.MONGO_BDBNAME || "filmDb";
+
+const MONGO_USER = process.env.MONGO_USER || null;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || null;
+
+const QUERY_STRING = MONGO_USER ?
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}` :
+    `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`;
+
+mongoose.connect(QUERY_STRING, { userNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        logger("Conexión establecida")
+    }).catch(error => {
+        console.log(error.masage)
+    });
+
+
+module.exports = filmDb;
