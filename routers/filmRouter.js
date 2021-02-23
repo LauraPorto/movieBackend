b88ec7693498bd659647f85f94eaa8e6bc955db9
Laura <lauraporto596@gmail.com>
2021-02-23 19:34:58 +0100

    CRUD of film checked

--- routers/filmRouter.js
+++ routers/filmRouter.js
@@ -5,11 +5,11 @@ const Film = require("../models/film");
 //ENDPOINTS CRUD
 
 //Traer todas las peliculas
-filmRouter.get('/allmovies', async(req, res) => {
+filmRouter.get('/films/allmovies', async(req, res) => {
     try {
         res.json(await filmController.indexAllFilms());
     } catch (error) {
-        return res.sendStatus(500).json({
+        return res.status(500).json({
             message: "Server Error"
         });
     }
@@ -21,58 +21,80 @@ filmRouter.get('/:id', async(req, res) => {
         const id = req.params.id;
         res.json(await filmController.findFilmById(id));
     } catch (error) {
-        return res.sendStatus(500).json({
+        return res.status(500).json({
             message: "Server Error"
         });
     };
 });
 
 //Traer películas por Título
-filmRouter.get('/title', async(req, res) => {
+filmRouter.get('/films/title/:title', async(req, res) => {
     try {
         const title = req.params.title;
-        res.json(await filmController.findByTitle(title));
+        let resultado = await filmController.findByTitle(title);
+        res.json(resultado);
     } catch (error) {
-        return res.sendStatus(500).json({
+        return res.status(500).json({
             message: "Server Error"
         });
     };
 });
 
+/* routerFilms.get('/films/title/:title', async(req, res) => {
+    try {
+        const zapato = req.params.title;
+
+        let resultado = await filmController.findOneFilm(zapato);
+
+        res.json(resultado)
+    } catch (error) {
+        return res.sendStatus(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+
+});
+ async findOneFilm(title) {
+        console.log('estamos aquiiiiii', title);
+        const titleFound = film.findOne({ title: title })
+        return titleFound;
+    }
+} */
+
 //Guardar/añadir una película (save)
 filmRouter.post('/add', async(req, res) => {
     try {
-        const id = await filmController.store(new Movie(req.body));
+        const movie = await filmController.store(req.body);
         const status = "success";
-        res.json({ status, id });
+        res.json({ status, movie });
     } catch (error) {
-        return res.sendStatus(500).json({
+        return res.status(500).json({
             message: "Server Error"
         });
     };
 });
 
 //Borrar una película
-filmRouter.delete('/:id', async(req, res) => {
+filmRouter.delete('/delete/:id', async(req, res) => {
     try {
-        const id = req.params.id;
+        const _id = req.params.id;
         const status = "deleted";
-        await filmController.destroyFilm(id);
-        res.json({ status, id });
+        let resultado = await filmController.destroyFilm(_id);
+        res.json({ status, resultado });
     } catch (error) {
-        return res.sendStatus(500).json({
+        return res.status(500).json({
             message: "Server Error"
         });
     };
 });
 
 //Modificar los datos de una película (update)
-filmRouter.put('/:id', async(req, res) => {
+filmRouter.put('/update/id:id', async(req, res) => {
     try {
-        const id = req.params.id;
-        res.json(await filmController.updateFilm(id, new Movie(req.body)))
+        const _id = req.params.id;
+        res.json(await filmController.updateFilm(_id, new Movie(req.body)))
     } catch (error) {
-        return res.sendStatus(500).json({
+        return res.status(500).json({
             message: "Server Error"
         });
     };
@@ -81,3 +103,40 @@ filmRouter.put('/:id', async(req, res) => {
 
 
 module.exports = filmRouter;
+
+
+
+/*
+
+const rentalSchema = {
+    ownerId: {
+        type: ObjectId,
+        required: true
+    }, 
+    dateIni: Date, 
+    dateEnd: Date,
+    movieReference: {
+        type: ObjectId, 
+        required: true
+    }
+};
+
+IN CONTROLLER
+Movie.find().limit(10).then(movies) => {
+    return movie.map (async (mmovie) => {
+        movie.owner = await User.findById(movie.owner);
+    })
+    return Movie.find.limit(10);
+};
+
+Esto es un modo de controlar la relación pedido-cliente. 
+Aunque podríamos prescindir de él simplemente con el modelo rental
+
+*/
+
+/*
+
+router.user('/:id/cars') -- le digo que use el router de car.
+Recurso car del usuario ID.
+Así, me llevan a los cars de un usuario simpleente escribiendo esto al final del routes de car. Así, de manera anidada, podríamos acceder por URL mediante Postman.
+*/
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,39 +2,47 @@ const userRouter = require("express").Router();
 const userController = require("../controllers/userController");
 const User = require("../models/user");
 
+
+//ENDPOINTS CRUD
+
+//Todos los usuarios
 userRouter.get('/allUsers', async(req, res) => {
     try {
         res.json(await userController.indexAllUsers());
     } catch (error) {
-        return sendStatus(500).json({
+        return status(500).json({
             message: "Server Error"
         });
     };
 });
 
+
+//Usuario por ID
 userRouter.get('/userId', async(req, res) => {
     try {
         const id = req.params.id;
         res.json(await userController.findUserById(id));
     } catch (error) {
-        return sendStatus(500).json({
+        return status(500).json({
             message: "Server Error"
         });
     };
 });
 
+//Registro usuario
 userRouter.post('/register', async(req, res) => {
     try {
         const id = res.json(await userController.register(new User(req.body)));
         const status = "success";
         res.json({ id, status });
     } catch (error) {
-        return sendStatus(500).json({
+        return status(500).json({
             message: "Server Error"
         });
     };
 });
 
+//Borrar usuario por ID
 userRouter.delete('/:id', async(req, res) => {
     try {
         const id = req.params.id;
@@ -42,12 +50,13 @@ userRouter.delete('/:id', async(req, res) => {
         await UserController.destroyUser(id);
         res.json({ id, status });
     } catch (error) {
-        return sendStatus(500).json({
+        return status(500).json({
             message: "Server Error"
         });
     };
 });
 
+//Modificar datos usuario
 userRouter.put('/:id', async(req, res) => {
     try {
         const id = req.params.id;
@@ -55,7 +64,7 @@ userRouter.put('/:id', async(req, res) => {
         await userController.updateUser(id, new Client(req.body));
         res.json({ id, status });
     } catch (error) {
-        return sendStatus(500).json({
+        return status(500).json({
             message: "Server Error"
         });
     };
